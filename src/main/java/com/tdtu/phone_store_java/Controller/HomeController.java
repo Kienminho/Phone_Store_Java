8a0b49eaf048aad22373289487ab8dc119e53380
Trần Hợp Kiên <hopkien1609@gmail.com>
2023-12-17 21:47:31 +0700

    countinue setup api, response, utils

---
+++ src/main/java/com/tdtu/phone_store_java/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.tdtu.phone_store_java.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class HomeController {
+    @GetMapping("/")
+    public String GetLogin() {
+        return "login";
+    }
+}
---
+++ src/main/resources/static/js/login.js
@@ -0,0 +1,52 @@
+const btnLogin = $(".btn-login");
+const username = $("#email");
+const password = $("#password");
+const errorMessage = $(".message-errors");
+
+//xử lý đăng nhập
+btnLogin.on("click", () => {
+    if (validateInfoLogin(username.val(), password.val())) {
+        const data = {
+            username: username.val(),
+            password: password.val(),
+        };
+
+        fetch("/auth/handleLogin", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                if (res.statusCode === 200 || res.statusCode === 304) {
+                    window.location.href = res.data.urlRedirect;
+                } else {
+                    password.val("");
+                    displayErrorMessage(res.message);
+                }
+            })
+            .catch((err) => {
+                username.val("");
+                password.val("");
+                displayErrorMessage("Lỗi hệ thống vui lòng thử lại sau ít phút.");
+            });
+    }
+    else {
+        displayErrorMessage("Vui lòng không được bỏ trống tên hoặc mật khẩu.");
+    }
+});
+
+function validateInfoLogin(username, password) {
+    return !(username === "" || password === "");
+
+}
+
+function displayErrorMessage(message) {
+    errorMessage.text(message).fadeIn("slow");
+    setTimeout(function () {
+        errorMessage.fadeOut("slow");
+    }, 3000);
+}
