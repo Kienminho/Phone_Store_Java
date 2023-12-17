e5673250ef939302d953b3212131f58a236450a3
Trần Hợp Kiên <hopkien1609@gmail.com>
2023-12-17 20:23:48 +0700

    set up env

---
+++ src/main/java/com/tdtu/phone_store_java/PhoneStoreJavaApplication.java
@@ -0,0 +1,13 @@
+package com.tdtu.phone_store_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneStoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneStoreJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdtu/phone_store_java/PhoneStoreJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdtu.phone_store_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneStoreJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
